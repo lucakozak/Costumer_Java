b7efc5a5bc992ecb88d1dacfce973f41579e485d
lucakozak <32192842+lucakozak@users.noreply.github.com>
2018-04-20 13:16:54 +0200

    Update Costumer.java

--- Customer_Java/src/Costumer/Costumer.java
+++ Customer_Java/src/Costumer/Costumer.java
@@ -1,5 +1,75 @@
 package Costumer;
 
-public class Costumer {
+public  class Costumer {
+	
+	private String name;
+	private int tax;
+	private int postCode;
+	private int shopNumber;
+	private String email;
+	
+	public Costumer(String name, int tax, int postCode, int shopNumber, String email) {
+		this.name = name;
+		this.tax = tax;
+		this.postCode = postCode;
+		this.shopNumber = shopNumber;
+		this.email = email;
+	}
+	
+	public Costumer(String name2, String email2, int tax2, int postCode2) {
+		// TODO Auto-generated constructor stub
+	}
 
+	public String toString() {
+		return "Name:" + name + ", Tax:" + tax + ", Post code:" + postCode + ", Shop number:" + shopNumber+ ", Email:" + email;
+	}
+
+	public  String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getTax() {
+		return tax;
+	}
+	
+	public void setTax(int tax) {
+		this.tax = tax;
+	}
+	
+	public int getPostCode() {
+		return postCode;
+	}
+	
+	public void setPostCode(int postCode) {
+		this.postCode = postCode;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public int getShopNumber() {
+		return shopNumber;
+	}
+	
+	public void setShopNumber(int shopNumber) {
+		this.shopNumber = shopNumber;
+	}
+
+
+	public boolean getRegularCostumer() {
+		if (this.shopNumber > 100)
+			return true;
+
+		return false;
+	}
 }
+
