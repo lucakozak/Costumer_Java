f505a697076da1820bbfa774de774917136b9603
lucakozak <32192842+lucakozak@users.noreply.github.com>
2018-04-20 13:17:50 +0200

    Update CostumerStore.java

--- Customer_Java/src/Costumer/CostumerStore.java
+++ Customer_Java/src/Costumer/CostumerStore.java
@@ -1,5 +1,28 @@
 package Costumer;
 
-public class CostumerStore {
+public class CostumerStore extends Costumer {
+	
+	public enum type {
+		RETAILER, WHOLESALER, OTHER
+	};
+
+	private type costumerType;
+
+	public CostumerStore (String name, String email, int tax, int postCode, int shopNumer, type costumerType) {
+		super(name, email, tax, postCode);
+		this.costumerType = costumerType;
+	}
+
+	public String toString() {
+		return super.toString() + " style: " + this.costumerType;
+	}
+
+	public type getCostumerType() {
+		return costumerType;
+	}
+
+	public void setCostumerType(type costumerType) {
+		this.costumerType = costumerType;
+	}
 
 }
